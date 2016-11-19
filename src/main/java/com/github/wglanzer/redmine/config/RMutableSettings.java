869bf9adfb6d62d9530a9744ec8a0855da66dcf1
w.glanzer <wernerglanzer@googlemail.com>
2016-11-19 21:31:21 +0100

    Add TestingSource for easier debug

--- src/main/java/com/github/wglanzer/redmine/config/RMutableSettings.java
+++ src/main/java/com/github/wglanzer/redmine/config/RMutableSettings.java
@@ -2,9 +2,11 @@ package com.github.wglanzer.redmine.config;
 
 import com.github.wglanzer.redmine.config.model.RSourceBean;
 import com.github.wglanzer.redmine.model.ISource;
+import com.google.common.base.Strings;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import javax.swing.*;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
@@ -18,7 +20,7 @@ import java.util.List;
 public class RMutableSettings implements ISettings, Serializable
 {
 
-  private static final long serialVersionUID = 7208863115811988902L;
+  private static final long serialVersionUID = 7208863115811985902L;
 
   private List<RSourceBean> sources;
 
@@ -26,12 +28,39 @@ public class RMutableSettings implements ISettings, Serializable
   @Override
   public List<ISource> getSources()
   {
-    return sources != null ? new ArrayList<>(sources) : new ArrayList<>();
+    ArrayList<ISource> sources = this.sources != null ? new ArrayList<>(this.sources) : new ArrayList<>();
+    if(Strings.nullToEmpty(System.getProperty("plugin.redmine.debug")).equals("true"))
+    {
+      _TestingSource tt = new _TestingSource();
+      if(sources.stream().noneMatch(pSource -> pSource.getURL().equals(tt.getURL())))
+        sources.add(tt);
+    }
+    return sources;
   }
 
   public void setSources(@Nullable List<RSourceBean> pSources)
   {
     sources = pSources;
+  }
+
+  /**
+   * Source for testing-purposes
+   */
+  private static class _TestingSource extends RSourceBean
+  {
+    @NotNull
+    @Override
+    public String getURL()
+    {
+      return "http://redmine.zero-division.de";
+    }
+
+    @NotNull
+    @Override
+    public String getAPIKey()
+    {
+      return "dd76c1ab88e4b3ba1e7a53228818a852eecde987";
+    }
   }
 
 }
