6d087a998427278af2eb5263c416d03b0986154a
w.glanzer <w.glanzer@adito.de>
2016-12-13 08:32:28 +0100

    Server does not use source.isCheckCertificate()

--- core/src/main/java/com/github/wglanzer/redmine/model/impl/server/PollingServer.java
+++ core/src/main/java/com/github/wglanzer/redmine/model/impl/server/PollingServer.java
@@ -37,7 +37,7 @@ public class PollingServer implements IServer
   {
     taskCreator = pTaskCreator;
     source = pSource;
-    connection = RRestConnectionBuilder.createConnection(pLoggingFacade, source.getURL(), source.getAPIKey(), source.getPageSize(), true);
+    connection = RRestConnectionBuilder.createConnection(pLoggingFacade, source.getURL(), source.getAPIKey(), source.getPageSize(), source.isCheckCeritifacte());
     loggingFacade = pLoggingFacade;
     directory = new PollingProjectDirectory(connection);
     executor = new PollingExecutor(pLoggingFacade, pTaskCreator, () ->
